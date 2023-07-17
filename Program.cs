28a5df4b8730c56cf58dd243b4169dcc0221264a
Lê Quốc Thước <102453854+thuocle@users.noreply.github.com>
2023-07-17 21:44:56 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Hocsinh_MVC.Controller;
+using Hocsinh_MVC.View;
+using Hocsinh_MVC.Model;
+using Hocsinh_MVC.Helper;
+namespace Hocsinh_MVC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HocSinhView view = new HocSinhView();
+            view.MENU();
+        }
+    }
+}
