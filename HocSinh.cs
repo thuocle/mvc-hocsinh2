f2d6202eed8d2415b749ebafa2f43d77ecd7b2e7
Lê Quốc Thước <102453854+thuocle@users.noreply.github.com>
2023-07-17 21:45:49 +0700

    Add files via upload

---
+++ HocSinh.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hocsinh_MVC.Helper;
+namespace Hocsinh_MVC.Model
+{
+    class HocSinh
+    {
+        #region public proprety
+        public int MaHS { get; set; }
+        public string TenHS { get; set; }
+        public DateTime NgaySinh { get; set; }
+
+        #endregion
+
+        #region Constructor
+        public HocSinh()
+        {
+             
+            MaHS = InputHelper.InputINT(Resource.InpIDHS, Resource.ErrID);
+            TenHS = InputHelper.InputSTR(Resource.InpTenHS, Resource.ErrTenHS, 6, 20);
+            NgaySinh = InputHelper.InputDATE(Resource.InpNgSinh, Resource.ErrNgSinh, new DateTime(2005,01,01), new DateTime(2016, 12,31));
+            
+        }
+
+        #endregion
+        #region Public Method
+        public void InThongTin()
+        {
+            Console.WriteLine($"Ma hoc sinh: {MaHS} co ten la {TenHS} - sinh ngay {NgaySinh}");
+        }
+        #endregion
+    }
+}
